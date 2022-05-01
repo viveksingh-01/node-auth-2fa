249a84323c1906e349351e4213009378ca196a11
viveksingh-01 <viveksingh9301@gmail.com>
2022-05-01 11:31:46 +0530

    wip: create User entity

---
+++ src/entity/User.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ name: "first_name" })
+  firstName!: string;
+
+  @Column({ name: "last_name" })
+  lastName!: string;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column()
+  password!: string;
+}
