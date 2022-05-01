93e728c1bd5850ed332fb62de9dfcecf904c164b
viveksingh-01 <viveksingh9301@gmail.com>
2022-05-01 09:43:35 +0530

    wip: create http-server using express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello!");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Welcome to Two-Factor Authentication using Node.js");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started and listening at port ${PORT}`);
+});
